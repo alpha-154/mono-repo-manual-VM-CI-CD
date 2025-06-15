e5685b74cbbeedcfc30d3260d3b9983fd06dddeb
alpha-154 <mrhrakib07@gmail.com>
2025-06-16 01:23:06 +0600

    initial commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "hi there!",
+  });
+});
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+
+  res.json({
+    message: "signup successfull!",
+    id: user.id,
+  });
+});
+
+app.listen(3002, () => {
+  console.log("server listening on the port 3002");
+});
